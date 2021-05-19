1987e0c3cf8e6efc333bd2550cfec6275f46185f
Opasd <78478987+Opasd@users.noreply.github.com>
2021-05-19 16:20:27 +0700

    remove redundant breaks in passwordgenerator

--- PasswordGenerator.cpp
+++ PasswordGenerator.cpp
@@ -9,32 +9,24 @@ std::uniform_int_distribution<int> LOW_LET_LIMIT(10, 35);
 std::uniform_int_distribution<int> UP_LET_LIMIT(36, 61);
 std::uniform_int_distribution<int> SPEC_SYM_LIMIT(63, 84);
 
-
 inline bool PasswordGenerator::IsValidInt(int num, bool limit_shift)
 {
     return (num >= (limit_shift == true ? 1 : 0) && num <= INT_MAX - 1);
 }
-
-
 
 char PasswordGenerator::GenerateRandomChar(CHAR_TYPE char_type)
 {
     switch (char_type) {
     case CHAR_TYPE::UP_CHAR:
         return CHARS_ARRAY[UP_LET_LIMIT(rnd_gen)];
-        break;
     case CHAR_TYPE::LOW_CHAR:
         return CHARS_ARRAY[LOW_LET_LIMIT(rnd_gen)];
-        break;
     case CHAR_TYPE::NUM_CHAR:
         return CHARS_ARRAY[NUM_LET_LIMIT(rnd_gen)];
-        break;
     case CHAR_TYPE::SPEC_CHAR:
         return CHARS_ARRAY[SPEC_SYM_LIMIT(rnd_gen)];
-        break;
     default:
         return '0'; // must be impossible because it only accepts CHAR_TYPE....
-        break;
     }
 }
 
@@ -43,19 +35,14 @@ char PasswordGenerator::GenerateMaskChar(char symbol)
     switch (symbol) {
     case 'L':
         return GenerateRandomChar(CHAR_TYPE::LOW_CHAR);
-        break;
     case 'U':
         return GenerateRandomChar(CHAR_TYPE::UP_CHAR);
-        break;
     case 'N':
         return GenerateRandomChar(CHAR_TYPE::NUM_CHAR);
-        break;
     case 'S':
         return GenerateRandomChar(CHAR_TYPE::SPEC_CHAR);
-        break;
     default:
         return symbol;
-        break;
     }
 }
 
@@ -65,7 +52,7 @@ std::string PasswordGenerator::GeneratePassword()
     if (m_mask.empty()) {
         pass.resize(length + 1);
         if (m_random_length == true) {
-            limit_for_random = std::uniform_int_distribution<int>(1,length);
+            limit_for_random = std::uniform_int_distribution<int>(1, length);
             pass.resize(limit_for_random(rnd_gen));
         }
         for (auto& x : pass)
@@ -77,7 +64,6 @@ std::string PasswordGenerator::GeneratePassword()
             pass[a] = GenerateMaskChar(m_mask[a]);
         return pass;
     }
-
 }
 
 void PasswordGenerator::SetPasswordMask(const std::string& mask)
@@ -104,5 +90,4 @@ void PasswordGenerator::SetPasswordSeed(int se)
 void PasswordGenerator::UseRandomPasswordLength(bool random_length)
 {
     m_random_length = random_length;
-    
 }
