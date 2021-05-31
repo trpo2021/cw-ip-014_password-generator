9967415209c1edc328e70ca372074c5d7abff614
Prophet <29117146+prophetl33t@users.noreply.github.com>
2021-05-31 20:37:35 +0700

    Fix linux compilation (missing alglorithm header)

--- PasswordGenerator.h
+++ PasswordGenerator.h
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <random>
 #include <vector>
+#include <algorithm>
 
 enum class CHAR_TYPE { up_char, low_char, num_char, spec_char };
 enum class ML_MODE { random = 1, forward, backward };
